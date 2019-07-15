fe367fd3139be580f645d67538f058750061bd6e
Ryan Chase <in.rchase@gmail.com>
2019-07-15 13:56:52 -0700

    add pageview gathering scripts

---
+++ scripts/get-wiki-pageviews.js
@@ -0,0 +1,21 @@
+const fs = require('fs')
+const mkdirp = require('mkdirp')
+const pageviews = require('pageviews')
+const d3 = require('d3')
+
+// Getting aggregated pageviews for a single project
+pageviews
+  .getAggregatedPageviews({
+    project: 'en.wikipedia',
+    agent: 'user',
+    granularity: 'daily',
+    start: '2015070100',
+    end: '2018062600'
+  })
+  .then(result => {
+    // console.log(result.items)
+    const output = d3.csvFormat(result.items)
+    mkdirp('./output');
+    fs.writeFileSync('./output/wiki-pageviews.csv', output)
+  })
+  .catch(console.error)
---
+++ scripts/join-people.js
@@ -0,0 +1,69 @@
+const fs = require('fs')
+const mkdirp = require('mkdirp')
+const d3 = require('d3')
+const outputDir = './output/people-joined'
+
+const wikiPageviewData = d3.csvParse(
+  fs.readFileSync('./output/wiki-pageviews.csv', 'utf-8')
+)
+
+function calculateTraffic({views, timestamp}) {
+  const match = wikiPageviewData.find(d => d.timestamp)
+
+  if (match) return views / match.views
+  console.error('no match', ppd)
+  return null
+}
+
+function joinData(person) {
+  const id = person.link.replace('/wiki/', '')
+
+  // TODO delete
+  console.log(id)
+
+  const personPageviewData = d3.csvParse(
+    fs.readFileSync(`./output/people-pageviews/${id}.csv`, 'utf-8')
+  )
+
+  console.log(personPageviewData)
+  // person
+  // name, link, YoB, YoD, DoD, description
+
+  // ppd
+  // project, article, granularity, timestamp, access, agent, views
+
+  const merged = personPageviewData.map(ppd => ({
+    ...ppd,
+    ...person,
+    percent_traffic: calculateTraffic(ppd)
+  }))
+
+  // Remove unecessary fields
+  merged.forEach(d => {
+    delete d.article
+    delete d.granularity
+    delete d.access
+    delete d.agent
+  })
+
+  const output = d3.csvFormat(merged)
+  fs.writeFileSync(`${outputDir}/${id}.csv`, output
+}
+
+function init() {
+  mkdirp(outputDir)
+
+  const data = d3.csvParse(
+    fs.readFileSync('./output/all-deaths-2015-2018.csv', 'utf-8')
+  )
+
+  // TODO
+  // we can make this data.map(joinData) if we don't want logging
+  data.forEach((d, i) => {
+    console.log(`${i} of ${data.length}`)
+    joinData(d);
+  })
+}
+
+
+init()
